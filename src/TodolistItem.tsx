2cdaf2d023d3fb0a423bc8adb939881689fe8432
ElenaWebDev984 <badalova.dev.984@gmail.com>
2026-01-03 14:20:05 +0100

    create TodolistItem

---
+++ src/TodolistItem.tsx
@@ -0,0 +1,39 @@
+export type TodolistItemType = {
+    title: string
+    tasks: TaskType[]
+}
+
+export type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
+export const TodolistItem = ({title, tasks}: TodolistItemType) => {
+    return (
+        <div>
+            <h3>{title}</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                <li>
+                    <input type="checkbox" checked={tasks[0].isDone}/> <span>{tasks[0].title}</span>
+                </li>
+                <li>
+                    <input type="checkbox" checked={tasks[1].isDone}/> <span>{tasks[1].title}</span>
+                </li>
+                <li>
+                    <input type="checkbox" checked={tasks[2].isDone}/> <span>{tasks[2].title}</span>
+                </li>
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+        </div>
+    );
+};
+
